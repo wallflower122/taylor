1144f0a16837baad656945ed6195630d025e80c6
wallflower122 <87909309+wallflower122@users.noreply.github.com>
2021-08-20 00:35:24 +0530

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// fadeout stuff
+
+$(".taylorswift").click(function(){
+ $(".taylorswifticon").fadeOut(100).fadeIn(200);
+});
+$(".fearless").click(function(){
+ $(".fearlessicon").fadeOut(100).fadeIn(200);
+});
+$(".speaknow").click(function(){
+ $(".speaknowicon").fadeOut(100).fadeIn(200);
+});
+$(".red").click(function(){
+ $(".redicon").fadeOut(100).fadeIn(200);
+});
+$(".pop").click(function(){
+ $(".popicon").fadeOut(100).fadeIn(200);
+});
+$(".reputation").click(function(){
+ $(".reputationicon").fadeOut(100).fadeIn(200);
+});
+$(".lover").click(function(){
+ $(".lovericon").fadeOut(100).fadeIn(200);
+});
+$(".folklore").click(function(){
+ $(".folkloreicon").fadeOut(100).fadeIn(200);
+});
+$(".evermore").click(function(){
+ $(".evermoreicon").fadeOut(100).fadeIn(200);
+});
+$(".title-image").mouseover(function(){
+  $(".title-image").fadeOut(100).fadeIn(200);
+});
+$(".fa-apple").click(function(){
+  $(".fa-apple").fadeOut(100).fadeIn(200);
+});
